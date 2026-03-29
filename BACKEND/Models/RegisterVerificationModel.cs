df8f793a07b6fca96fb2e0a3d78b0a4ce370eb20
jerald783 <jeraldtulibao783@gmail.com>
2026-03-29 13:09:41 +0800

    Delete BACKEND directory

--- BACKEND/Models/RegisterVerificationModel.cs
+++
@@ -1,8 +0,0 @@
-public class RegisterVerificationModel
-{
-    public string? FullName { get; set; }
-    public string? Email { get; set; }
-    public string? Password { get; set; }
-    public string? Otp { get; set; }
-    public int? RoleId { get; set; }
-}
--- BACKEND/Models/ResetPasswordModel.cs
+++
@@ -1,5 +0,0 @@
-public class ResetPasswordModel
-{
-    public string?  Token { get; set; }
-    public string? NewPassword { get; set; }
-}
